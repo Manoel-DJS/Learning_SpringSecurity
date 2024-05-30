4556929431a24b60c717fd74e529319198efa5c7
Manoel-DJS <94149934+Manoel-DJS@users.noreply.github.com>
2024-05-30 11:08:35 -0300

    Method GetMapping for feed

--- src/main/java/tech/buildrun/springsecurity/controller/TweetController.java
+++ src/main/java/tech/buildrun/springsecurity/controller/TweetController.java
@@ -1,11 +1,15 @@
 package tech.buildrun.springsecurity.controller;
 
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 import tech.buildrun.springsecurity.controller.dto.CreateTweetDto;
+import tech.buildrun.springsecurity.controller.dto.FeedDto;
+import tech.buildrun.springsecurity.controller.dto.FeedItemDto;
 import tech.buildrun.springsecurity.entities.Role;
 import tech.buildrun.springsecurity.entities.Tweet;
 import tech.buildrun.springsecurity.repository.TweetRepository;
@@ -23,6 +27,19 @@ public class TweetController {
     public TweetController(TweetRepository tweetRepository, UserRepository userRepository) {
         this.tweetRepository = tweetRepository;
         this.userRepository = userRepository;
+    }
+
+    @GetMapping("/feed")
+    public ResponseEntity<FeedDto> feed(@RequestParam(value = "page", defaultValue = "0") int page,
+                                        @RequestParam(value = "pageSize", defaultValue = "10") int pageSize){
+
+        var tweets = tweetRepository.findAll(
+                PageRequest.of(page, pageSize, Sort.Direction.DESC, "creationTimestamp"))
+                .map(tweet ->
+                        new FeedItemDto(tweet.getTweetId(), tweet.getContent(),
+                                tweet.getUser().getUsername()));
+        return ResponseEntity.ok(new FeedDto(tweets.getContent(), page, pageSize, tweets.getTotalPages(), tweets.getTotalElements()));
+
     }
 
     @PostMapping("/tweets")
